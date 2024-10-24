6ede7939ac55465c7ae17644ce3e598059781095
Chan Onn <chanonn0103@gmail.com>
2024-10-25 03:46:54 +0800

    edited cors

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 
 //app.use(cors());
 app.use(cors({
-    origin: ['http://localhost:5173']
+    origin: 'http://localhost:5173'
 }));
 app.use(bodyParser.json());
 
