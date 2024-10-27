96c210b8e42552ce66c2a6d64f7bea50f7e099ff
Chan Onn <chanonn0103@gmail.com>
2024-10-27 18:33:55 +0800

    added vercel cors

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: 'https://farm-management-app-navy.vercel.app/'
 }));
 app.use(bodyParser.json());
 
